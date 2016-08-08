60fac86fb8cc29206c27702cdbd598ec97fa9480
root <root@localhost.localdomain>
2016-08-09 08:53:27 +0900

    hihi

--- install_bootstrap.sh
+++ install_bootstrap.sh
@@ -1,17 +1,72 @@
 #!/bin/bash
+
+index=0
+echo -n "input network interface: "
+read interfaceVar
+array[$index]=$interfaceVar
+((index+=1))
+
+echo -n "input each password:"
+read password
+array[$index]=$password
+((index+=1))
+
+echo -n "input ssh port:"
+read portNum
+array[$index]=$portNum
+((index+=1))
+
+masterIpNum=0
+
+while true; do
+echo -n "input master ip (press q to quit):"
+read masterip
+
+if [ "$masterip" == "q" ]; then
+ break
+else
+ array[$index]=$masterip
+ ((masterIpNum+=1))
+ ((index+=1))
+fi
+
+done
+
+
+while true; do
+echo -n "input worker ip(press q to quit):"
+read value
+
+if [ "$value" == "q" ]; then
+ break
+else
+ array[$index]=$value
+ ((index+=1))
+fi
+
+done
+
+arrlen=${#array[@]}
+
+for(( i=3+$masterIpNum; i<$arrlen; i++))
+do
+ echo ${array[$i]}
+done
+
 yum -y update
 yum install -y vim
 yum install -y net-tools
 yum install -y wget
 
 echo -e "\n" | ssh-keygen -t rsa -N ""
-
 wget http://apt.sw.be/redhat/el7/en/x86_64/rpmforge/RPMS/sshpass-1.05-1.el7.rf.x86_64.rpm
 
 rpm -Uvh sshpass-1.05-1.el7.rf.x86_64.rpm
 
-sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no root@ip
-sshpass -p "123" ssh-copy-id -o StrictHostKeyChecking=no root@ip
+for((i=3+$masterIpNum;i<$arrlen; i++)
+do
+sshpass -p "{$array[1]}]" ssh-copy-id -o StrictHostKeyChecking=no root@{$array[$i]}
+done
 
 
 cat > /etc/yum.repos.d/docker.repo << '__EOF__'
@@ -70,43 +125,52 @@ cd /dcos
 curl -O https://downloads.dcos.io/dcos/EarlyAccess/commit/14509fe1e7899f439527fb39867194c7a425c771/dcos_generate_config.sh
 cd genconf
 
-cat > ./config.yaml << "EOF"
----
-agent_list:
-- 192.168.1.34
+echo "---" >> config.yaml
+
+for(( i=3+$masterIpNum; i<$arrlen; i++))
+do
+echo "- ${array[$i]}" >> config.yaml
+done
+
+cat >> config.yaml << "EOF"
 bootstrap_url: 'file:///opt/dcos_install_tmp'
 cluster_name: dcos
 exhibitor_storage_backend: static
 master_discovery: static
 master_list:
-- 192.168.1.75
+EOF
+
+for(( i=3; i<3+$masterIpNum; i++))
+do
+echo "- ${array[$i]}" >> config.yaml
+done
+
+
+
+cat >> config.yaml << "EOF"
 resolvers:
 - 8.8.4.4
 - 8.8.8.8
 ssh_key_path: /genconf/ssh_key
-ssh_port: 22
-ssh_user: root
 EOF
 
-cat > ./ip-detect << "EOF"
-#!/bin/bash
-set -o nounset -o errexit
-export PATH=/usr/sbin:/usr/bin:$PATH
-echo $(ip addr show enp0s3 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-EOF
+echo "ssh_port: $portNum" >> config.yaml
+echo "ssh_user: root"
+echo "oauth_enabled: 'false'"
+
+
+echo "#!/bin/bash" >> ip-detect
+echo "set -o nounset -o errexit" >> ip-detect
+echo "export PATH=/usr/sbin:/usr/bin:\$PATH" >> ip-detect
+echo "echo \$(ip addr show ${array[0]} | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)" >> ip-detect
+
 
 chmod 777 ip-detect config.yaml
 cp /root/.ssh/id_rsa /dcos/genconf/ssh_key && chmod 0600 /dcos/genconf/ssh_key
 
 
-bash dcos_generate_config.sh --genconf
-bash dcos_generate_config.sh --install-prereqs
-bash dcos_generate_config.sh --preflight
-bash dcos_generate_config.sh --deploy
-bash dcos_generate_config.sh --postflight
-
-
-
-
-
-
+#bash dcos_generate_config.sh --genconf
+#bash dcos_generate_config.sh --install-prereqs
+#bash dcos_generate_config.sh --preflight
+#bash dcos_generate_config.sh --deploy
+#bash dcos_generate_config.sh --postflight
